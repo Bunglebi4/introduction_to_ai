aa19e578bf537a3952b1ef8cbe28bed8d698d985
A-Shulegin <102576914+A-Shulegin@users.noreply.github.com>
2024-02-10 00:17:30 +0300

    add def move def print_state def child_nodes

--- dfs.py
+++ dfs.py
@@ -1,11 +1,8 @@
 from structures import *
-
-def print_state(state):
-    for i in range(3):
-        print(state[i])
-
+        
 start = Node(action=None)
-next = Action.up(start)
-print_state(start.state)
-print('--------')
-print_state(next.state)
+print_state(start)
+print("------------")
+for node in child_nodes(start):
+    print_state(node)
+    print("------------")
--- structures.py
+++ structures.py
@@ -2,6 +2,7 @@ import collections
 from copy import deepcopy
 from collections import deque
 
+#Класс узла
 class Node:
     def __init__(self, action: str, parent=None, depth=0, state=None, idx=(2, 2)) -> None:
         self.state = state if state else [[6, 2, 8], [4, 1, 7], [5, 3, 0]]
@@ -11,76 +12,56 @@ class Node:
         self.depth = depth
         self.zero_idx = idx
 
-
-class Action:
-    @staticmethod
-    def right(node):
-        state = deepcopy(node.state)
-        i, j = node.zero_idx[0], node.zero_idx[1]
-        if j > 1:
+#Функция передвижения нуля
+def move(direction, node):
+    state = deepcopy(node.state)
+    i, j = node.zero_idx[0], node.zero_idx[1]
+    match direction:
+        case "right":
+            if j > 1:
+                return None
+            j += 1 
+            state[i][j - 1] = state[i][j]
+        case "left":
+            if j < 1:
+                return None
+            j -= 1
+            state[i][j + 1] = state[i][j]
+        case "down":
+            if i > 1:
+                return None
+            i += 1
+            state[i - 1][j] = state[i][j]
+        case "up":
+            if i < 1:
+                return None
+            i -= 1
+            state[i + 1][j] = state[i][j]
+        case _:
             return None
-        j += 1
-        state[i][j - 1] = state[i][j]
-        state[i][j] = 0
-        next_node = Node(
-            state=state,
-            parent=node,
-            action="right",
-            depth=node.depth + 1,
-            idx=(i, j)
-        )
-        return next_node
 
-    @staticmethod
-    def left(node):
-        state = deepcopy(node.state)
-        i, j = node.zero_idx[0], node.zero_idx[1]
-        if j < 1:
-            return None
-        j -= 1
-        state[i][j + 1] = state[i][j]
-        state[i][j] = 0
-        next_node = Node(
-            state=state,
-            parent=node,
-            action="left",
-            depth=node.depth + 1,
-            idx=(i, j)
-        )
-        return next_node
 
-    @staticmethod
-    def down(node):
-        state = deepcopy(node.state)
-        i, j = node.zero_idx[0], node.zero_idx[1]
-        if i > 1:
-            return None
-        i += 1
-        state[i - 1][j] = state[i][j]
-        state[i][j] = 0
-        next_node = Node(
-            state=state,
-            parent=node,
-            action="down",
-            depth=node.depth + 1,
-            idx=(i, j)
-        )
-        return next_node
+    state[i][j] = 0
+    next_node = Node(
+        state=state,
+        parent=node,
+        action=direction,
+        depth=node.depth + 1,
+        idx=(i, j)
+    )
+    return next_node
 
-    @staticmethod
-    def up(node):
-        state = deepcopy(node.state)
-        i, j = node.zero_idx[0], node.zero_idx[1]
-        if i < 1:
-            return None
-        i -= 1
-        state[i + 1][j] = state[i][j]
-        state[i][j] = 0
-        next_node = Node(
-            state=state,
-            parent=node,
-            action="up",
-            depth=node.depth + 1,
-            idx=(i, j)
-        )
-        return next_node
+#Вывод состояния узла
+def print_state(node):
+    state = node.state
+    for i in range(3):
+        print(state[i])
+
+#Поиск всех детей узла
+def child_nodes(node):
+    child_nodes_list = []
+    for direction in ["left", "right", "up", "down"]:
+        next_node = move(direction, node)
+        if next_node != None:
+            child_nodes_list.append(next_node)
+    return child_nodes_list

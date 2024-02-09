2d8bd4594ec3485f7060e7ac880cfee1d04fbc66
Bunglebi4 <102578128+Bunglebi4@users.noreply.github.com>
2024-02-09 22:23:19 +0300

    Create structures.py

---
+++ structures.py
@@ -0,0 +1,31 @@
+class State:
+    """
+    matrix 3X3
+    """
+    pass
+class ParentNode:
+    pass
+class Depth:
+    '''
+    скорее всего можно записать как поле
+    '''
+    pass
+class Action:
+    @staticmethod
+    def right():
+        pass
+
+    @staticmethod
+    def left():
+        pass
+
+    @staticmethod
+    def down():
+        pass
+
+    @staticmethod
+    def up():
+        pass
+
+class Tree:
+    pass

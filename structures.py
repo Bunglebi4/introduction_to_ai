64cd979daefad44ed884dda7c1d966f17f4564e2
Bunglebi4 <102578128+Bunglebi4@users.noreply.github.com>
2024-02-09 23:23:08 +0300

    added base structures

--- structures.py
+++ structures.py
@@ -1,31 +1,81 @@
-class State:
-    """
-    matrix 3X3
-    """
-    pass
-class ParentNode:
-    pass
-class Depth:
-    '''
-    скорее всего можно записать как поле
-    '''
-    pass
+import collections
+from collections import deque
+
+class Node:
+    def __init__(self, action: str, parent = None, depth=0, state=None, idx=(2, 2)) -> None:
+        self.state = state if state else [[6, 2, 8], [4, 1, 7], [5, 3, 0]]
+        self.parent = parent
+        self.action = action
+        self.cost = 0
+        self.depth = depth
+        self.zero_idx = idx
+
+
 class Action:
     @staticmethod
-    def right():
-        pass
+    def right(state, node):
+        i, j = node.zero_idx[0], node.zero_idx[1]
+        if j > 1:
+            return None
+        j += 1
+        state[i][j - 1] = state[i][j]
+        state[i][j] = 0
+        next_node = Node(
+            state=state,
+            parent=node,
+            action="right",
+            depth=node.depth + 1,
+            idx=(i, j)
+        )
+        return next_node
 
     @staticmethod
-    def left():
-        pass
+    def left(state, node):
+        i, j = node.zero_idx[0], node.zero_idx[1]
+        if j < 1:
+            return None
+        j -= 1
+        state[i][j + 1] = state[i][j]
+        state[i][j] = 0
+        next_node = Node(
+            state=state,
+            parent=node,
+            action="left",
+            depth=node.depth + 1,
+            idx=(i, j)
+        )
+        return next_node
 
     @staticmethod
-    def down():
-        pass
+    def down(state, node):
+        i, j = node.zero_idx[0], node.zero_idx[1]
+        if i > 1:
+            return None
+        i += 1
+        state[i - 1][j] = state[i][j]
+        state[i][j] = 0
+        next_node = Node(
+            state=state,
+            parent=node,
+            action="down",
+            depth=node.depth + 1,
+            idx=(i, j)
+        )
+        return next_node
 
     @staticmethod
-    def up():
-        pass
-
-class Tree:
-    pass
+    def up(state, node):
+        i, j = node.zero_idx[0], node.zero_idx[1]
+        if i < 1:
+            return None
+        i -= 1
+        state[i + 1][j] = state[i][j]
+        state[i][j] = 0
+        next_node = Node(
+            state=state,
+            parent=node,
+            action="up",
+            depth=node.depth + 1,
+            idx=(i, j)
+        )
+        return next_node
